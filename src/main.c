e2a46cd8b3c93ec4fb5c386a18679608fddebaed
Eros Mendonça <erosmendonca@outlook.com>
2024-05-02 20:41:00 +0100

    feat: rendering players

--- src/main.c
+++ src/main.c
@@ -4,77 +4,50 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-const int SCREEN_WIDTH = 48;
-const int SCREEN_HEIGHT = 16;
-const int PLAYERS_HEIGHT = 4;
-const int PLAYER_DISTANCE_FROM_WALL = 2; 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int PLAYERS_HEIGHT = 120;
+const int PLAYERS_WIDTH = 16;
+const int PLAYER_DISTANCE_FROM_WALL = 16;
+
+SDL_Renderer* renderer = NULL;
 
 int player1Position = 2;
 int player2Position = 2;
 
+struct Ball {
+  float x;
+  float y;
+} ball;
 
+struct Player {
+  float x;
+  float y;
+} p1, p2;
 
-int calculatePlayerPosition () {
+int calcPlayerVerticalPosition () {
   int screenHalf = SCREEN_HEIGHT / 2;
   int playerHeightHalf = PLAYERS_HEIGHT / 2;
   return screenHalf - playerHeightHalf;
 }
 
-int getPlayerEnd(int playerStart) {
-  return playerStart + PLAYERS_HEIGHT;
+void initializePlayers() {
+  p1.x = PLAYER_DISTANCE_FROM_WALL;
+  p1.y = calcPlayerVerticalPosition();
+
+  p2.x = SCREEN_WIDTH - (PLAYER_DISTANCE_FROM_WALL + PLAYERS_WIDTH);
+  p2.y = calcPlayerVerticalPosition();
 }
 
-bool isPlayerRow(int currentRow) {
-  int player1End = getPlayerEnd(player1Position);
-  int player2End = getPlayerEnd(player2Position);
-  bool isPlayer1Row = player1Position <= currentRow && player1End >= currentRow;
-  bool isPlayer2Row = player2Position <= currentRow && player2End >= currentRow;
-  return isPlayer1Row || isPlayer2Row;
-}
+void drawPlayers() {
+  SDL_Rect player1 = {p1.x, p1.y, PLAYERS_WIDTH, PLAYERS_HEIGHT};
+  SDL_Rect player2 = {p2.x, p2.y, PLAYERS_WIDTH, PLAYERS_HEIGHT};
 
-bool isPlayerColumn(int currentColumn) {
-  int startWall = 1;
-  int endWall = SCREEN_WIDTH - 2;
-  int player1Col = startWall + PLAYER_DISTANCE_FROM_WALL;
-  int player2Col = endWall - PLAYER_DISTANCE_FROM_WALL;
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set color to red (R,G,B,A)
+  SDL_RenderFillRect(renderer, &player1);
 
-  return currentColumn == player1Col || currentColumn == player2Col;
-}
-
-bool isPlayerCell(int currentColumn, int currentRow) {
-  return isPlayerColumn(currentColumn) && isPlayerRow(currentRow);
-}
-
-void clearScreen() {
-  system("cls");
-}
-
-void update() {
-  player1Position = player1Position + 1;;
-}
-
-void draw() {
-  clearScreen();
-  for(int i = 0; i < SCREEN_HEIGHT; i++) { 
-    bool isFirstRow = i == 0; 
-    bool isLastRow = i == (SCREEN_HEIGHT - 1); 
-    for(int j = 0; j < SCREEN_WIDTH; j++) {
-      bool isFirstColumn = j == 0;
-      bool isLastColumn = j == (SCREEN_WIDTH - 1);
-      if(
-          isFirstRow    || 
-          isLastRow     || 
-          isFirstColumn ||
-          isLastColumn ||
-          isPlayerCell(j, i)
-        ) {
-        printf("#");
-      } else {
-        printf(" ");
-      }
-    }
-    printf("\n");
-  }
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Set color to blue (R,G,B,A)
+  SDL_RenderFillRect(renderer, &player2);
 }
 
 // void handleKeyPress(SDL_Event sdlEvent) {
@@ -93,6 +66,11 @@ void draw() {
 //   }
 // }
 
+void cleanup() {
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
 int main() {
   // Initialize SDL
   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -105,8 +83,8 @@ int main() {
     "Pong", 
     SDL_WINDOWPOS_CENTERED, 
     SDL_WINDOWPOS_CENTERED, 
-    640, 
-    480, 
+    SCREEN_WIDTH, 
+    SCREEN_HEIGHT, 
     0
   );
 
@@ -115,30 +93,41 @@ int main() {
     return -1;
   }
 
-  // Get window surface
-  SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-
-  if(!screenSurface) {
-    printf("Window surface could not be created! SDL_Error: %s\n", SDL_GetError());
-    return -1;
+  // Create renderer
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer) {
+      printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+      return -1;
   }
+
+  initializePlayers();
 
   bool quit = false;
 
   // Main loop
   while(!quit) {
     SDL_Event e;
-    
+
     // Handle events on queue
     while(SDL_PollEvent(&e) > 0) {
       // User requests quit
       if(e.type == SDL_QUIT) {
         quit = true;
       }
-  
-      SDL_UpdateWindowSurface(window);
     }
+
+    // Clear the renderer
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw the players
+    drawPlayers();
+
+    // Update the screen
+    SDL_RenderPresent(renderer);
   }
+
+  cleanup();
 }
 
 
